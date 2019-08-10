e48e3f5c8cf68ad6740c0a55093c99896aff3c03
Beata <bea.swirko@gmail.com>
2019-08-10 12:32:29 +0200

    PowRec

---
+++ src/main/java/Pow2/MainPow.java
@@ -0,0 +1,10 @@
+package Pow2;
+
+public class MainPow {
+
+    public static void main(String[] args) {
+        TwoPow twoPow = new TwoPow();
+        System.out.println(twoPow.powIter(5));
+        System.out.println(twoPow.powRec(5));
+    }
+}
